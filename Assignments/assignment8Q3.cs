5a8babbce4a335ee11203fd48825ac458acf7679
sudheervarma5920 <sudheervarma987654@gmail.com>
2024-07-22 12:09:16 +0530

    q3 file added

---
+++ Assignments/assignment8Q3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Assignment8Q3
+{
+    public class Error
+    {
+       
+        public void Prog(string path)
+        {
+            try
+            {
+                int a, b;
+                Console.WriteLine("Enter A and B values: ");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (Exception e)
+            {
+                File.AppendAllText(path, Environment.NewLine + e.Message);
+            }
+        }
+    }
+    internal class Errormessage
+    {
+        public static void Main()
+        {
+            Error error = new Error();
+            //error.create("D:\\Work\\Aadarsh_Assignments\\Aadarsh_Assignments");
+            error.Prog("F:\\Work\\sudheer_Assignments\\sudheer_Assignments\\practice3.txt");
+        }
+    }
+}
